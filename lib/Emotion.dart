3996c9b8d88a852b625a011cb6811c8e7f33fcfe
YuyiMiao <cxmiao2001@gmail.com>
2020-11-20 17:51:27 -0600

    Finished the basic functions including input and display of data.

---
+++ lib/Emotion.dart
@@ -0,0 +1,31 @@
+import 'package:intl/intl.dart';
+
+class Emotion {
+  final int id;
+  final double amount;
+  final DateTime date;
+  final String category;
+
+  String get formattedDate {
+    var formatter = new DateFormat('yyyy-MM-dd');
+    return formatter.format(this.date);
+  }
+
+  static final columns = ['id', 'amount', 'date', 'category'];
+
+  Emotion(this.id, this.amount, this.date, this.category);
+
+  factory Emotion.fromMap (Map<String, dynamic> data) {
+    return Emotion(data['id'],
+        data['amount'],
+        DateTime.parse(data['date']),
+        data['category']);
+  }
+
+  Map<String, dynamic> toMap() => {
+    "id": id,
+    "amount": amount,
+    "date": date.toString(),
+    "category": category
+  };
+}
